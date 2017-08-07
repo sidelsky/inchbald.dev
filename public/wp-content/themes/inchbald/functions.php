e2cdc78134442dee72f45595b140710c187acbcb
Errol Sidelsky <errol@squie.com>
2017-08-07 14:45:25 +0100

    Get functions function

--- public/wp-content/themes/inchbald/functions.php
+++ public/wp-content/themes/inchbald/functions.php
@@ -1,35 +1,19 @@
 <?php
-  //Wordpress reset (removes redundant scripts)
-  require('functions/reset.php');
 
-  //Enqueue scripts, styles etc.
-  require('functions/enqueues.php');
+    /**
+    * Require all functions within the functions folder
+    */
+    function getFunctions() {
 
-  //Utilities
-  require('functions/utilities.php');
+        $folder = '/functions/*.php';
+        $files = glob(dirname(__FILE__) . $folder);
 
-  //Helper functions
-  require('functions/helpers.php');
+        foreach( $files as $file ) {
+            require_once( $file );
+        }
 
-  //Project specific
-  require('functions/project.php');
+    }
 
-  //Hide user toolbar
-  require('functions/remove-admin-bar.php');
+    getFunctions();
 
-  //Custom GF spinner
-  require('functions/custom-form-spinner.php');
-
-  //Move GF script to footer
-  require('functions/gf-js-to-footer.php');
-
-  //Redirect home
-  require('functions/redirect.php');
-
-
-  //Custom Post Types
-  //require('functions/custom-post-types.php');
-
-  //Custom Taxonomies
-  //require('functions/custom-taxonomies.php');
 ?>
